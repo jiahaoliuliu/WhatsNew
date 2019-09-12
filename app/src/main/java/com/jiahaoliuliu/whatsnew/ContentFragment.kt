dcf8fe5480a05b26f07471e79ee28034685c2757
Jiahao Liu <jiahao.liu@dnata.com>
2019-09-12 14:20:03 +0400

    Improved the UI by expanding the image

--- app/src/main/java/com/jiahaoliuliu/whatsnew/ContentFragment.kt
+++ app/src/main/java/com/jiahaoliuliu/whatsnew/ContentFragment.kt
@@ -38,41 +38,6 @@ class ContentFragment : Fragment() {
         return view
     }
 
-//    // TODO: Rename method, update argument and hook method into UI event
-//    fun onButtonPressed(uri: Uri) {
-//        listener?.onFragmentInteraction(uri)
-//    }
-
-//    override fun onAttach(context: Context) {
-//        super.onAttach(context)
-//        if (context is OnFragmentInteractionListener) {
-//            listener = context
-//        } else {
-//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-//        }
-//    }
-//
-////    override fun onDetach() {
-//        super.onDetach()
-//        listener = null
-//    }
-
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     *
-//     *
-//     * See the Android Training lesson [Communicating with Other Fragments]
-//     * (http://developer.android.com/training/basics/fragments/communicating.html)
-//     * for more information.
-//     */
-//    interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        fun onFragmentInteraction(uri: Uri)
-//    }
-//
     companion object {
         /**
          * Use this factory method to create a new instance of
